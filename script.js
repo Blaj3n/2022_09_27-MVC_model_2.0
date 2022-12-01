ed05b43bfc33676aaea614d54666b1235c7251a3
Bláj3n <ulrichbence15@freemail.hu>
2022-12-01 18:15:28 +0100

    névtelen jquery függvény

---
+++ script.js
@@ -0,0 +1,7 @@
+//itt fogjuk példányosítani az osztályainkat
+//itt fogjuk definiálni a dokument ready eseményhez tartozó névtelen jquery függvényt
+
+//a program belépési pontja
+$(function () {
+
+});
